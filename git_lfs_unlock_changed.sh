e4bd41cb96ec57a97d965e9ea26bc05956749157
Tobias Mueller <tobias.mu@protonmail.com>
2022-08-07 23:15:35 +0200

    Made git_lfs_unlock_changed.sh more efficient

--- git_lfs_unlock_changed.sh
+++ git_lfs_unlock_changed.sh
@@ -1,38 +1,48 @@
 #!/usr/bin/env bash
 
+##Remember that it is nececarry to use following fetch-depth in github action:
+# - name: Check out repository code
+#        uses: actions/checkout@v3
+#        with:
+#          fetch-depth: 0
+
 ALL_COMMITS=$(git rev-list "HEAD" 2>&1) && exit_status=$? || exit_status=$?
 LATEST_REMOTE_COMMIT=$(git rev-parse "@~" 2>&1) && exit_status=$? || exit_status=$?
+LATEST_REMOTE_COMMIT=f283721ba00aa5c804d826db26baa77930f0e9e5
 
-echo "LOOP:"
+# This sets all the Authors of file in a string delimited by " " in var AUTHORS_$escaped_filename
+# exp.: Authors for file test/test.py are in va AUTHORS_test_test_py = foo.bar@asdfa.com fizzbuzz@foo.com
 for COMMIT in $ALL_COMMITS
 do
     if [ $COMMIT != $LATEST_REMOTE_COMMIT ]; then
-        #TODO: As git lock information fetching is really slow that should be minimized
-        # For this please group files so every file will only be checked once.
-        # Even better: only make one 'git lfs locks' call
-        echo "Commit: $COMMIT"
-        echo "$(git show $COMMIT | grep Author |head -1)"
         COMMIT_FILES=$(git diff-tree --no-commit-id --name-only -r $COMMIT)
-        echo "Files:"
         for COMMIT_FILE in $COMMIT_FILES
         do
-            echo "File: $COMMIT_FILE"
-            COMMIT_FILE_LOCK=$(git lfs locks -p $COMMIT_FILE)
-            if [ -n "${COMMIT_FILE_LOCK}" ]; then
-                echo "  Is Locked"
-                COMMIT_FILE_LOCK_OWNER=$(echo $COMMIT_FILE_LOCK | tr -s \" \" | cut -d ' ' -f2)
-                # FIXME: MAP REMOTE SERVER USERNAME to AUTHOR
-                # https://docs.github.com/en/rest/users/users#get-a-user
-                # https://github.com/git-lfs/git-lfs/issues/3578
-                # 
-                # PS: Getting the commiter/pusher github username seems to be impossible
-                echo "  Locked by: $COMMIT_FILE_LOCK_OWNER"
-                #TODO: ADD flag to simply unlock all files regardless of commiter and current lock_owner (Default: Check if author == lock_owner)
+            COMMIT_FILE_AUTHOR=$(git show $COMMIT | grep Author | cut -d ' ' -f4 | head -1 | sed 's/^<\(.*\)>$/\1/')
+            escaped_filename=$(echo $COMMIT_FILE | tr ./ _)
+            x=AUTHORS_$escaped_filename
+            val=$(echo "${!x} $COMMIT_FILE_AUTHOR")
+            if [ -z "${!x}" ]; then
+                val=$(echo "$COMMIT_FILE_AUTHOR")
             else
-                echo "  Is not locked"
+                if [[ "$COMMIT_FILE_AUTHOR" == *"${!x}"* ]]; then
+                    continue
+                fi
             fi
+            declare $x="$val"
         done
     else
         break
     fi
 done
+
+GIT_LFS_FILE_LOCKS=$(git lfs locks)
+while IFS= read -r GIT_LFS_FILE_LOCK; do
+    LOCKED_FILE=$(echo $GIT_LFS_FILE_LOCK | tr -s \" \" | cut -d ' ' -f1)
+    LOCK_OWNER=$(echo $GIT_LFS_FILE_LOCK | tr -s \" \" | cut -d ' ' -f2)
+    # FIXME: MAP REMOTE SERVER USERNAME to AUTHOR
+    # https://docs.github.com/en/rest/users/users#get-a-user
+    # https://github.com/git-lfs/git-lfs/issues/3578
+
+    echo "Locked File: $LOCKED_FILE Owned by: $LOCK_OWNER"
+done <<< "$GIT_LFS_FILE_LOCKS"
